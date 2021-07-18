bca6727ec3ae9c4da9bdf2f0271f4952fffe31f4
nguyenluc99 <yeuma99@gmail.com>
2021-07-18 09:35:52 +0700

    update weight

--- Project/train.py
+++ Project/train.py
@@ -8,16 +8,19 @@ from keras.optimizers import RMSprop
 from keras.utils.vis_utils import plot_model
 from keras.preprocessing.image import img_to_array
 from sklearn.preprocessing import MultiLabelBinarizer
+from sklearn.model_selection import train_test_split
 from keras import backend as K
 from myModel.model import VGGNet
 import matplotlib.pyplot as plt
 from imutils import paths
 import numpy as np
+import pandas as pd
 import argparse
 import random
 import pickle
 import cv2
 import os
+import glob
 
 #%%
 # construct the argument parse and parse the arguments (for command line)
@@ -42,24 +45,61 @@ LR = 1e-3 #Learning rate 0.001
 img_dim = (45,45,3)
 train_data_dir = 'splited_dataset/train'
 test_data_dir = 'splited_dataset/test'
+#train_data_dir
 labels = []
 #Nbr of training images
-train_samples_nbr  = sum(len(files) for _, _, files in os.walk(r'splited_dataset/train'))
+#train_samples_nbr  = sum(len(files) for _, _, files in os.walk(r'splited_dataset/train'))
 #Nbr of testing images
-test_samples_nbr  = sum(len(files) for _, _, files in os.walk(r'splited_dataset/test'))
+#test_samples_nbr  = sum(len(files) for _, _, files in os.walk(r'splited_dataset/test'))
 
 #%%
 # Infos about our Dataset
 nbr_of_pictures = []
+data_dir = "data/extracted_images"
 
-labels = os.listdir("data/extracted_images")
+labels = os.listdir(data_dir)
+
+train_df = dict()
+train_df['path'] = []
+train_df['label'] = []
+
+test_df = dict()
+test_df['path'] = []
+test_df['label'] = []
+
+print(len(glob.glob(data_dir + os.sep + "*/*")))
+
+for folder in labels:
+    fol = data_dir + os.sep + folder + os.sep + "*.jpg" 
+    paths = glob.glob(fol)
+    train, test = train_test_split(paths, test_size=0.2, random_state=42)
+    for p in train:
+        train_df['path'].append(p)
+        train_df['label'].append(folder)
+        #train_df.append({'path': p, 'label': fol}, ignore_index=True)
+    for p in test:
+        test_df['path'].append(p)
+        test_df['label'].append(folder)
+        #test_df.append({'path': p, 'label': fol}, ignore_index=True) 
+
+
+train_df = pd.DataFrame(train_df)
+test_df = pd.DataFrame(test_df)
+print("TRAIN:", train_df.size)
+print("TEST :", test_df.size)
+print(train_df)
+print(test_df)
+train_samples_nbr = train_df.size / 2
+test_samples_nbr = test_df.size / 2
+
 for _, _, files in os.walk(r'data/extracted_images'):
     nbr_of_pictures.append(len(files))
 
 nbr_of_pictures=nbr_of_pictures[1:]
 #print nbr of pictures in every class
 print("Number of samples in every class ...")
-for i in range(82):  # 82 : Nbr of classes
+print(len(labels))
+for i in range(60):  # 82 : Nbr of classes
     print(labels[i]," : ",nbr_of_pictures[i])
 #%%
 # Checking image data format
@@ -68,10 +108,6 @@ if K.image_data_format() == 'channels_first':
     input_shape = (img_dim[2], img_dim[0], img_dim[1])
 else:
     input_shape = (img_dim[0], img_dim[1], img_dim[2])
-
-#%%
-
-
 
 #%%
 
@@ -86,7 +122,7 @@ labels = mlb.fit_transform(labels)
 # Building the model 
 model = VGGNet.build(
 	width=img_dim[1], height=img_dim[0],
-	depth=img_dim[2], classes=82,
+	depth=img_dim[2], classes=55, # classes = 82
     activFct="softmax") #for multi-class classification
 model.summary()
 print('Number of layers of our model : ',len(model.layers))
@@ -123,18 +159,43 @@ test_datagen = ImageDataGenerator(rescale=1. / 255)
 
 #%%
 
-train_generator = train_datagen.flow_from_directory(
-    train_data_dir,
-    target_size=(img_dim[0], img_dim[1]),
-    batch_size=BS,
-    class_mode='categorical')
+#train_generator = train_datagen.flow_from_directory(
+#    train_data_dir,
+#    target_size=(img_dim[0], img_dim[1]),
+#    batch_size=BS,
+#    class_mode='categorical')
+#
+#validation_generator = test_datagen.flow_from_directory(
+#    test_data_dir,
+#    target_size=(img_dim[0], img_dim[1]),
+#    batch_size=BS,
+#    class_mode='categorical')
+#
 
-validation_generator = test_datagen.flow_from_directory(
-    test_data_dir,
-    target_size=(img_dim[0], img_dim[1]),
-    batch_size=BS,
-    class_mode='categorical')
 
+
+train_generator = train_datagen.flow_from_dataframe(
+        dataframe=train_df,
+        directory=os.getcwd(),
+        x_col='path',
+        y_col='label',
+        batch_size=BS,
+        seed=42,
+        shuffle=True,
+        target_size=(img_dim[0], img_dim[1]),
+        class_mode='categorical')
+
+validation_generator = test_datagen.flow_from_dataframe(
+        dataframe=test_df,
+        directory=os.getcwd(),
+        x_col='path',
+        y_col='label',
+        batch_size=BS,
+        seed=42,
+        shuffle=True,
+        target_size=(img_dim[0], img_dim[1]),
+        class_mode='categorical')
+print("DONE CREATING GENERATOR")
 #%%
 # Training
 history = model.fit_generator(
@@ -152,7 +213,7 @@ history = model.fit_generator(
 print("Saving the model...")
 #model.save(args["model"])
 model.save("model.model")
-model.save_weights("weights.h5")
+model.save_weights("weights_new.h5")
 #save the multi-label binarizer to disk
 print("Saving Labels...")
 # f = open(args["labelbin"], "wb")
